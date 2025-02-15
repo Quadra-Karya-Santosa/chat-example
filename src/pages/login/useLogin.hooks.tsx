af946b647327a87c63ac132e21b11ebf34afdeb1
dionisius7 <dionisiusadityaoctanugraha@gmail.com>
2025-02-16 00:56:16 +0700

    fix: remove unused navigate at login hooks

--- src/pages/login/useLogin.hooks.tsx
+++ src/pages/login/useLogin.hooks.tsx
@@ -1,22 +1,18 @@
 import { useState } from "react";
-import { useNavigate } from "react-router";
 import { useAppDispatch } from "../../store";
 import { saveTokenAuth } from "../../store/auth";
 import { loginUser } from "../../utils/api";
-import { ChatPath } from "../chat/chat.page";
 
 const useLogin = () => {
   const dispatch = useAppDispatch()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [loginError, setLoginError] = useState("")
-  const navigate = useNavigate()
 
   const handleLogin = async (email: string, password: string) => {
     try {
       const userData = await loginUser(email, password)
       dispatch(saveTokenAuth(userData))
-      navigate(ChatPath);
     } catch (error) {
       console.error("Login failed:", error)
       setLoginError("Login failed. Please try again.")
